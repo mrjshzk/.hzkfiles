f7a1936f49d215de9bbd8b18087d221db3f12e94
Miguel Silva <mrjsilva05@gmail.com>
2025-12-26 02:43:37 +0000

    some things

---
+++ scripts/open_tui.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# ~/scripts/launch-tui.sh
+kitty --class floating-tui --title "$1" -e "$@"
---
+++ scripts/reload_waybar.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+#if pgrep waybar:
+#killall waybar ; waybar & disown; dunstify "Reloaded Waybar"
+output=$(pgrep waybar)
+if [ ! -z "${output}" ]; then
+    killall waybar
+fi
+# stdout to one file, stderr to another
+waybar > stderr.txt
